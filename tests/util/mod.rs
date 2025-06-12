349a087593890571ec8c8fc36d96ba8d3376e099
Dominic Clifton <me@dominicclifton.name>
2025-06-13 01:56:42 +0200

    Use a feature for env_logger to try and reduce the MSRV.

--- tests/util/mod.rs
+++ tests/util/mod.rs
@@ -3,7 +3,10 @@ pub mod testing {
 
     /// You can enable trace logging by setting the RUST_LOG environment variable to "trace", e.g. "RUST_LOG=trace"
     /// See: https://docs.rs/env_logger/0.9.1/env_logger/#capturing-logs-in-tests
+    ///
+    /// Requires "env_logger" feature.
     pub fn logging_init() {
+        #[cfg(feature = "env_logger")]
         let _ = env_logger::builder().is_test(true).try_init();
     }
 }
